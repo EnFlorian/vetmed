06f2f61b603c98201335676c4f149faeb68488e5
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-08-24 18:50:46 +0200

    Fix: Fixed typos

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import { Footer, Navbar } from "./components/layout";
-import { Home } from "./pages";
+import { Home, NotFound } from "./pages";
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </div>
