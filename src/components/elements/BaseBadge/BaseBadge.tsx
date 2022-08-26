7fc306af13df5841ead94d43af1057119f6b499b
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-08-26 21:53:53 +0200

    Feat: Added badge component

--- src/components/elements/BaseBadge/BaseBadge.tsx
+++ src/components/elements/BaseBadge/BaseBadge.tsx
@@ -1,7 +1,13 @@
+import { Children } from "react";
 import "./BaseBadge.scss";
 
-const BaseBadge = () => {
-  return <div>BaseBadge</div>;
+interface IBaseBadgeProps {
+  title: string;
+  children?: React.ReactNode;
+}
+
+const BaseBadge = ({ title, children }: IBaseBadgeProps) => {
+  return <div className="base-badge">{children || title}</div>;
 };
 
 export default BaseBadge;
