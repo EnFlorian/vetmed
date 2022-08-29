8e3f89f7bbbc7dc80166547c310706e662aac411
Enflorian <33962956+EnFlorian@users.noreply.github.com>
2022-08-29 10:00:49 +0200

    Fix: Fixed margin issues when expaneded

--- src/components/elements/Accordion/Accordion.tsx
+++ src/components/elements/Accordion/Accordion.tsx
@@ -15,17 +15,20 @@ const Accordion = ({ title, content }: IAccordionProps) => {
   return (
     <div className="accordion">
       <div className="accordion__header" onClick={toggle}>
-        <div className="accordion__icon">{isOpen ? <FaMinus /> : <FaPlus />}</div>
+        <div className="accordion__icon-wrapper">
+          <div className="accordion__icon">{isOpen ? <FaMinus /> : <FaPlus />}</div>
+        </div>
         <h3 className="accordion__title">{title}</h3>
       </div>
       <AnimatePresence>
         <motion.div
           initial={{ height: 0 }}
           animate={{ height: isOpen ? "auto" : 0 }}
+          transition={{ duration: 0.5 }}
           exit={{ height: 0 }}
           className="accordion__content"
         >
-          <p>{content}</p>
+          <p className="accordion__text">{content}</p>
         </motion.div>
       </AnimatePresence>
     </div>
