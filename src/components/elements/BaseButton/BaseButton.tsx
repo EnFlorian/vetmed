bd9ca625d22f64c70cc83a6e11c0167dad7a0d2b
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-08-29 02:06:07 +0200

    Fix: Added missing click handler

--- src/components/elements/BaseButton/BaseButton.tsx
+++ src/components/elements/BaseButton/BaseButton.tsx
@@ -3,11 +3,16 @@ import "./BaseButton.scss";
 interface IBaseButtonProps {
   title: string;
   children?: React.ReactNode;
+  clickHandler?: () => void;
   type: "primary" | "secondary";
 }
 
-const BaseButton = ({ title, children, type }: IBaseButtonProps) => {
-  return <button className={`base-button base-button--${type}`}>{children || title}</button>;
+const BaseButton = ({ title, children, type, clickHandler }: IBaseButtonProps) => {
+  return (
+    <button onClick={clickHandler} className={`base-button base-button--${type}`}>
+      {children || title}
+    </button>
+  );
 };
 
 export default BaseButton;
