a7508f58e4dadff048388cb84b0cc8c8ae612116
Enflorian <33962956+EnFlorian@users.noreply.github.com>
2022-08-29 10:05:07 +0200

    Feat: Added testimonial section

--- src/components/layout/TestimonialSection/TestimonialSection.tsx
+++ src/components/layout/TestimonialSection/TestimonialSection.tsx
@@ -1,7 +1,24 @@
+import { BaseBadge } from "../../elements";
 import "./TestimonialSection.scss";
 
 const TestimonialSection = () => {
-  return <div>TestimonialSection</div>;
+  return (
+    <section className="testimonial-section">
+      <div className="testimonial-section__wrapper  container-md">
+        <header className="testimonial-section__header">
+          <BaseBadge title="Our Services" />
+          <h1 className="testimonial-section__title">What We can do for You</h1>
+          <p className="testimonial-section__description">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum euismod nisi eu nunc consectetur, eget
+            consectetur nisi euismod.
+          </p>
+        </header>
+        <main className="testimonial-section__main">
+          <TestimonalCard />
+        </main>
+      </div>
+    </section>
+  );
 };
 
 export default TestimonialSection;
