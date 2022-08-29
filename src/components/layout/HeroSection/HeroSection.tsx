3f57021e7a3e851e8da87ddca3d13eda926bc3dd
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-08-29 03:24:44 +0200

    Feat: Added content and slide indicators

--- src/components/layout/HeroSection/HeroSection.tsx
+++ src/components/layout/HeroSection/HeroSection.tsx
@@ -1,21 +1,21 @@
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Navigation } from "swiper";
-
 import "swiper/css";
 import "swiper/css/pagination";
-
 import "./HeroSection.scss";
-import catImage from "../../../assets/images/cat-1.jpg";
-import dogImage from "../../../assets/images/dog-6.jpg";
+import catImage from "../../../assets/images/hero-image-1.jpg";
+import dogImage from "../../../assets/images/hero-image-2.jpg";
+import { BaseBadge, BaseButton } from "../../elements";
 
 const HeroSection = () => {
   return (
     <div className="hero-section">
       <Swiper
-        spaceBetween={0}
+        spaceBetween={30}
+        allowTouchMove={false}
         centeredSlides={true}
         autoplay={{
-          delay: 2500,
+          delay: 30000,
           disableOnInteraction: false,
         }}
         navigation={true}
@@ -24,12 +24,19 @@ const HeroSection = () => {
         className="hero-section__slider"
       >
         <SwiperSlide>
-          <img className="hero-section__image" src={dogImage} alt="dog" />
+          <img draggable="false" className="hero-section__image" src={dogImage} alt="dog" />
         </SwiperSlide>
         <SwiperSlide>
-          <img className="hero-section__image" src={catImage} alt="dog" />
+          <img draggable="false" className="hero-section__image" src={catImage} alt="dog" />
         </SwiperSlide>
       </Swiper>
+      <div className="hero-section__content">
+        <div className="hero-section__content-inner">
+          <BaseBadge title="Welcome to Vetpet!" />
+          <h1 className="hero-section__title">We will keep your pet healthy and happy</h1>
+          <BaseButton title="Read More" type="primary" />
+        </div>
+      </div>
     </div>
   );
 };
