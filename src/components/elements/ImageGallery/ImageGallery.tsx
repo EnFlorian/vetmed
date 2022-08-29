3c544a6a81ec6ff4561621981ac6a49e8477751e
Enflorian <33962956+EnFlorian@users.noreply.github.com>
2022-08-29 10:01:13 +0200

    Feat: Added image gallery

--- src/components/elements/ImageGallery/ImageGallery.tsx
+++ src/components/elements/ImageGallery/ImageGallery.tsx
@@ -1,7 +1,28 @@
 import "./ImageGallery.scss";
 
-const ImageGallery = () => {
-  return <div>ImageGallery</div>;
+interface IGalleryImage {
+  path: string;
+  alt: string;
+}
+
+interface IImageGalleryProps {
+  images: IGalleryImage[];
+}
+
+const ImageGallery = ({ images }: IImageGalleryProps) => {
+  const renderedImages = images.map((image, index) => {
+    return (
+      <li key={index} className="image-gallery__image-wrapper">
+        <img src={image.path} alt="image" className="image-gallery__image" />
+      </li>
+    );
+  });
+
+  return (
+    <div className="image-gallery">
+      <ul className="image-gallery__list">{renderedImages}</ul>
+    </div>
+  );
 };
 
 export default ImageGallery;
