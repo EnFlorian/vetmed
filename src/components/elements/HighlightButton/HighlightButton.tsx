065b316fb08a762826941b048c0fe34693d894ea
EnFlorian <33962956+EnFlorian@users.noreply.github.com>
2022-08-29 02:06:36 +0200

    Style: Changed styling

--- src/components/elements/HighlightButton/HighlightButton.tsx
+++ src/components/elements/HighlightButton/HighlightButton.tsx
@@ -1,5 +1,20 @@
 import "./HighlightButton.scss";
+import pawImage from "../../../assets/paw-print-4.svg";
 
-const HighlightButton = () => {};
+interface IHighlightButtonProps {
+  title: string;
+  children?: React.ReactNode;
+  clickHandler?: () => void;
+}
+
+const HighlightButton = ({ title, children, clickHandler }: IHighlightButtonProps) => {
+  return (
+    <button onClick={clickHandler} className="highlight-button">
+      <img src={pawImage} alt="logo" className="highlight-button__logo" />
+
+      {children || title}
+    </button>
+  );
+};
 
 export default HighlightButton;
