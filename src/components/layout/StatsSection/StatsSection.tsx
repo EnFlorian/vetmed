b9c67ccf6e48e50e4e90a1966357eda969887db4
Enflorian <33962956+EnFlorian@users.noreply.github.com>
2022-09-09 11:15:18 +0200

    Style: Changed media queries

--- src/components/layout/StatsSection/StatsSection.tsx
+++ src/components/layout/StatsSection/StatsSection.tsx
@@ -12,15 +12,7 @@ const StatsSection = () => {
   return (
     <section className="stats-section" id="stats">
       <div className="stats-section__wrapper container-lg">
-        <div className="stats-section__left-content">
-          <h1 className="stats-section__title">We' Will Keep Your Pets Healthy And Happy</h1>
-          <p className="stats-section__description">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisi eget consectetur consectetur,
-          </p>
-        </div>
-        <div className="stats-section__right-content">
-          <ul className="stats-section__list">{statsCards}</ul>
-        </div>
+        <ul className="stats-section__list">{statsCards}</ul>
       </div>
     </section>
   );
